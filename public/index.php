2fa85363a6db9a927da516f38b2f7f2b87b15532
Nguyen Hoang Anh <nguyenhoanganh@MacBook-Air-cua-Nguyen.local>
2026-02-16 18:21:47 +0700

    fix: add mb_split polyfill for cpanel compatibility

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+if (!function_exists('mb_split')) {
+    function mb_split($pattern, $string, $limit = -1) {
+        return preg_split('/' . $pattern . '/u', $string, $limit);
+    }
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
